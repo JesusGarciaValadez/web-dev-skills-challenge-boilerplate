9c7ef08bfd9b41fe2e6f1df72aedcb36153f104d
JesusGarciaValadez <jesus.garciav@me.com>
2024-12-19 13:56:23 +0100

    feat(configuration): set-up initial configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,38 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureVite();
+    }
+
+    protected function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->environment('production')
+        );
+    }
+
+    protected function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(!$this->app->isProduction());
+        Model::unguard();
+    }
+
+    private function configureUrl(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
     }
 }
